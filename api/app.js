70cd07e6d8ae4e74ffc1d4dbca3ce4b3bb6c8702
nishenaryan14 <aryannishen27@gmail.com>
2024-04-21 22:29:38 +0530

    update 21/4

--- api/app.js
+++ api/app.js
@@ -34,6 +34,13 @@ app.use(
     },
   })
 );
+
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -95,22 +102,3 @@ app.use("/api/posts", postRoute);
 app.listen(PORT, () => {
   console.log(`Backend server is running on ${PORT}!`);
 });
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "public/images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, req.body.name);
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   try {
-//     return res.status(200).json("File uploded successfully");
-//   } catch (error) {
-//     console.error(error);
-//   }
-// });
